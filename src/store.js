0f40651d10be6e89849eb56985868c7fa37e8e42
Alejandro Valencia <alejandro.valencia@parkassist.com>
2020-06-17 19:23:23 -0300

    removed dev tools

--- src/store.js
+++ src/store.js
@@ -1,14 +1,8 @@
 /* eslint-disable no-underscore-dangle */
-import { compose, createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducer';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducer";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
